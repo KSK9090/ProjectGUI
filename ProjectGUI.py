1c3eb295d2ec3f265cbde385ff996fdd0e5b868f
Kemal Kaya <kemal.kaya@ymail.com>
2026-02-07 22:53:18 +0100

    Switched from pack to grid

--- ProjectGUI.py
+++ ProjectGUI.py
@@ -6,64 +6,74 @@ from tkinter import *
 gui = Tk()
 
 # ------------------- Hauptfenster -------------------
-gui.geometry("500x500")
+gui.geometry("700x500")
 gui.title("Kundenverzeichnis")
 gui.configure(bg="white")
+gui.columnconfigure(0, weight=1)
 
 brand = PhotoImage(file="brand.png")
-
-logo = PhotoImage(file="logo.png")
+logo = PhotoImage(file="brand.png")
 gui.iconphoto(True, logo)
 
-# ------------------- FRAMES -------------------
-header = Frame(gui, bg="grey", height=120)
-header.pack(fill="x")
+# ---- FUNKTIONEN ----
+def suchen():
+    pass
 
-content = Frame(gui, 
-                bg="white"
-                )
-content.pack(fill="both")
+# ---- HEADER ----
+header = Frame(gui, bg="grey")
+header.grid(row=0, column=0, columnspan=10, sticky="nsew")
+header.columnconfigure(0, weight=1)
 
-"""
-# Verschoben auf header frame
-skala_frame = LabelFrame(
-    gui, 
-    bg="white"
-    )
-skala_frame.pack()
-"""
-
-# ------------------- LOGO NE -------------------
+# ---- LOGO ----
 logo_label = Label(
     header,
     image=brand,
     bg="white"
 )
-logo_label.pack(side="right", padx=20, pady=10)
+logo_label.grid(row=0, column=1, rowspan=4, sticky="e", padx=20)
 
-# ------------------- LABELS INHALT -------------------
+# ---- SUCHFELD ----
 suchfeld_label = Label(
     header,
     text="Suchfeld:",
-    font=("Arial", 16, 'bold'),
+    font=("Arial", 14, 'bold'),
     bg="grey"
 )
-suchfeld_label.pack(padx=5, pady=5, anchor="nw")
+suchfeld_label.grid(row=0, column=0, sticky="w", padx=5)
 
 suchfeld_eingabe = Entry(
     header,
     font=("Arial", 12),
-    width=25
+    width=15
 )
-suchfeld_eingabe.pack(anchor="nw", padx=10, pady=5)  
+suchfeld_eingabe.grid(row=1, column=0, sticky="ew", pady=10, padx=5)
 
+# ---- SKALA ----
 skala = Scale(
     header, 
-    from_= 0, to = 50, 
+    from_= 0, to = 15, 
     orient = "horizontal",
-    label="Wie viele Ergebnisse sollen angezeigt werden?"
+    label="Maximale Ergebnisse",
+    width="15"
     )
 skala.set(0)
-skala.pack(fill="x", padx=10, pady=10)
+skala.grid(row=2, column=0, sticky="ew", pady=10, padx=5)
+
+# ---- BUTTON SUCHE ----
+suche_button = Button(header,
+                      command=suchen,
+                      text="suchen",
+                      font=14
+)
+suche_button.grid(row=3, column=0, pady=10)
+
+# ---- AUSGABE ----
+
+
+
+
+
+
+
 
 gui.mainloop()
